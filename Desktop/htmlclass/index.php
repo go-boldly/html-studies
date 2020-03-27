b2766ac9a768798d1891b0da013d2bfb147b06a3
Laurel Rumple <laurel.l.rumple@gmail.com>
2020-03-27 15:43:11 -0400

    changed index to php

---
+++ Desktop/htmlclass/index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<title>Laurel Rumple | Resume</title>
+		
+		<!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	</head>
+	
+	<body>
+	
+		<nav class="navbar navbar-default">
+	  <div class="container">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+		  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+			<span class="sr-only">Toggle navigation</span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		  </button>
+		  <a class="navbar-brand" href="index.html">Laurel Rumple</a>
+		</div>
+
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		  <ul class="nav navbar-nav">
+		   
+		  </ul>
+
+		  <ul class="nav navbar-nav navbar-right">
+			<li><a href="personal_statement.html">Personal Statement</a></li>
+			
+		  </ul>
+		</div><!-- /.navbar-collapse -->
+	  </div><!-- /.container-fluid -->
+	</nav>
+
+<div class="container">
+
+<!--Main Resume-->
+
+			<div class="panel panel-default">
+	  <div class="panel-heading">
+		<h3 class="panel-title"><strong>Sensory Spectrum</strong></h3>
+	  </div>
+	  
+	  <div class="panel-body">
+		<div class="col-md-8">
+		<strong>Data Analyst</strong><br />
+		<p align="justify">Collecting, organizing, and processing data with the use of RedJade, CPIMA, and Excel to ensure it is of the utmost quality and accuracy while presenting it in forms that allow it to be most useful for both colleagues and clients. Updating and maintaining company database.<br /></p></div>
+		<div class="col-md-4" align="right"><em>January, 2020 - Present</em></div>
+	  </div>
+	
+	
+		  <div class="panel-body">
+		<div class="col-md-8">
+		<strong>Sensory Analyst</strong><br />
+		<p align="justify">Descriptive analysis of personal care products using strict procedures and protocols set forth to guide the process of evaluating and scaling product attributes for further research, development, and marketing.</p></div>
+		<div class="col-md-4" align="right"><em>February, 2016 - Present</em></div>
+	  </div>
+	</div>
+	
+	<br /><br />
+	
+<!--Certifications and Skills-->
+	
+	<div class="panel panel-default">
+	  <div class="panel-heading">
+		<h3 class="panel-title"><strong>Certifications & Skills</strong></h3>
+	  </div>
+	  <div class="panel-body">
+		<div class="col-md-12"><p>Excel Certified</p><p>HTML Proficient</p><p>Database Management</p><p>RedJade</p><p>CPIMA</p></div>
+	</div>
+	</div>
+	
+	<br /><br />
+	<p align="center">&copy; Laurel Rumple - All Rights Reserved</p>
+
+</div>
+
+   <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+	
+	</body>
+</html>
